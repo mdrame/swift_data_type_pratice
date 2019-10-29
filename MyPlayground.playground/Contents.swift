efb55d9b2ee7c55b3776fd8e2461d0686b6f054d
mohammed <mdrame113@gmail.com>
2019-10-28 20:23:24 -0700

    closures

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,28 @@
+
+
+
 import UIKit
+
+// closures
+
+// closure that takes parameters and return value
+let driving = { (place: String) -> String in
+    return " I'm Driving in my sexy car going to \(place) "
+}
+// clalling closure is like calling a function
+let london = driving( "London")
+print(london)
+
+// passing a closure that takes parameter and return something  in a function as a parameter
+func travelDestination(closureHere closuree: (String) -> String) {
+    
+    
+    print(" Round Trip")
+    print(closuree(" Conakry "))
+    
+    
+}
+
+let location: Any = travelDestination(closureHere: driving)
 
 
