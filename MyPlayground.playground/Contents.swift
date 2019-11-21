33eb49141c4e3488b72541b5672cec9d160a8c99
mohammed <mdrame113@gmail.com>
2019-11-20 20:47:54 -0800

    collection types

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,36 +1,129 @@
+//
+//
+//
+//// tuples
+//
+//let results = ("404 Error | Page not found", 404)
+//// printing the first element of a turple
+//print(results.0)
+//
+//// optional Binding
+//var name : String?
+//
+//if let names = name {
+//    print("name is not optional")
+//} else {
+//    print("sorry name is a optional ")
+//}
+//
+//// errro handling
+//
+//func canThrowAnError() throws {
+//    // this function may or may not throw an error
+//
+//}
+//
+//do {
+//    try canThrowAnError()
+//    // no error wat caught
+//} catch {
+//    // an error was thrown
+//}
+//
+////Range operators / half rang ..< ( from to, but not included ) / One sided range ( 0... )
+//for i in 1...10 {
+//    print(i)
+//}
+//
+//var list = ["Mohammed", "Oumou"]
+//
+//print(list[0...])
+
+// logical operator
+
+//
+//let allowedEntry = false
+//
+//if !allowedEntry {
+//    print("ACCESS DENIED")
+//}
+//// Prints "ACCESS DENIED”
+//
+//let enteredDoorCode = true
+//let passedRetinaScan = false
+//if enteredDoorCode && passedRetinaScan {
+//    print("Welcome!")
+//} else {
+//    print("ACCESS DENIED")
+//}
+//// Prints "ACCESS DENIED”
 
 
 
-// tuples
 
-let results = ("404 Error | Page not found", 404)
-// printing the first element of a turple
-print(results.0)
+// Strings and characters ( Manupulation )
 
-// optional Binding
-var name : String?
+// escaping character
+//let quoit = "\" One Day I will become the richest man on earth\" - Mohammed "
+//print(quoit)
+//
+//// empty string
+//
+//let emptysTRING = " "
+//let anotherWay = String()
+//
+//// concatenating Strings
+//
+//var speak = "Hellow"
+//let exclamation: Character = "!"
+//
+//speak.append(exclamation)
+//print(speak)
 
-if let names = name {
-    print("name is not optional")
-} else {
-    print("sorry name is a optional ")
-}
+// string comparasing
 
-// errro handling
+//var testOne = "Mohammed"
+//var testTwo = "mohammed"
+//
+//if testOne == testOne {
+//    print("Same")
+//} else {
+//    print("Not Same")
+//}
 
-func canThrowAnError() throws {
-    // this function may or may not throw an error
-    
-}
 
-do {
-    try canThrowAnError()
-    // no error wat caught
-} catch {
-    // an error was thrown
-}
+// collection Type
 
-//Range operators / half rang ..< ( from to, but not included )
-for i in 1...10 {
-    print(i)
-}
+//var list = ["Mohammed", "Drame", "me", "Love", "You"]
+//print(list)
+//
+//if list.contains("Drame") {
+//    print("Yes")
+//} else {
+//    print("No")
+//}
+
+// dic
+var dic: [String: Int] = [String:Int]()
+
+dic["Mohammed"] = 24
+
+print(dic)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
