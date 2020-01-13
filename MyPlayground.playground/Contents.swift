99c1df0863af5db4dd3cead4dc97bd50c08a16ce
mohammed <mdrame113@gmail.com>
2020-01-13 12:07:39 -0800

    string manupulation

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,22 +2,40 @@
 import Foundation
 import UIKit
 
-var listToDublicate: [Character] = ["M", "i", "T", "T", "M", "i"]
+// Add dublicate using swift set
+//
+//var listToDublicate: [Character] = ["M", "i", "T", "T", "M", "i"]
+//
+//// Code in this playground is deleted almost every time I'm writting new code. {
+//
+//func addDublicate(list: [Character]) -> Set<Character> {
+//    // initialize set
+//    var sett: Set  = Set<Character>() // empty set
+//
+//    for charecters in list {
+//        sett.insert(charecters)
+//    }
+//
+//    return sett
+//
+//
+//}
+//
+//print(addDublicate(list: listToDublicate))
 
-// Code in this playground is deleted almost every time I'm writting new code. {
 
-func addDublicate(list: [Character]) -> Set<Character> {
-    // initialize set
-    var sett: Set  = Set<Character>() // empty set
-    
-    for charecters in list {
-        sett.insert(charecters)
-    }
-    
-    return sett
-    
-    
+
+
+// Strin g palindrome
+
+func acceptString(parameter: String) -> Bool {
+    let lowerCased = parameter.lowercased()
+    return lowerCased.reversed() == Array(lowerCased)
 }
 
 
-print(addDublicate(list: listToDublicate))
+
+acceptString(parameter: "Mohammed")
+
+
+
