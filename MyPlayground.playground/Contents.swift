aa903640d3ac717446566444793a7730ac7b2cf8
mohammed <mdrame113@gmail.com>
2020-02-12 21:33:12 -0800

    binary search

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,36 +5,44 @@
 //2. Swift String Manupulation
 //3. Other Data Type Manupulation (Array , Dic , Tuple, Set)
 
-// Binary Search
-
-var array = Array(1...100)
 
 
-func findLeterInArray(array list: [Int], key value: Int)-> Bool {
+// MARK: -> Binary Search
+
+var array = Array(0...50)
+
+
+/// Finding a key value in a array using binary search.
+func findLeterInArray(array list: [Int], key value: Int)->Bool{
     
-    if array.count == 0 { return false }
+    // making sure array has something inside
+    if list.count == 0 { return false }
     
-    let minIndex = 0
-    let maxIndex = array.count - 1
-    let midIndex = maxIndex / 2
-    let midValue = array[midIndex]
+    let minIndex = 0 // first index of array
+    let maxIndex = list.count - 1 // last index of array
+    let midIndex = maxIndex / 2 // middle index of array
+    let midValue = list[midIndex] // middle index value of array
     
-    if value < array[minIndex] || value > array[maxIndex] {
+    // testing to make sure key is with in the array
+    if value < list[minIndex] || value > list[maxIndex] {
         return false
     }
     
-    
+    // If key is greater then middle index of our array split array as the greater half and rerun function
     if value > midValue {
-        let slice = Array(array[midIndex + 1...maxIndex])
-        print(slice)
+        // create a new array of the upper half of our previous
+        var slice = Array(list[midIndex + 1...maxIndex])
+        print("UpWard \(slice)")
         return findLeterInArray(array: slice, key: value)
     }
     
     if value < midValue {
-        let slice = Array(array[minIndex...midIndex - 1])
+        var slice = Array(list[minIndex...midIndex - 1])
+        print("DownWard \(slice)")
         return findLeterInArray(array: slice, key: value)
     }
-    
+
+    // At any point in our code, if key is equal to middle return true
     if value == midValue {
         print("Key found ")
         return true
@@ -43,9 +51,8 @@ func findLeterInArray(array list: [Int], key value: Int)-> Bool {
     return false
 }
 
-
-
-print(findLeterInArray(array: array, key: 40))
+//
+print(findLeterInArray(array: array, key: 10))
 
 
 
